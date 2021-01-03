9f2d177cd5ac6bbb715809a3ffa6528ce4692090
Joep Hinderink <hello@joephinderink.nl>
2021-01-03 18:01:34 +0100

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Joep Hinderink on 03/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
